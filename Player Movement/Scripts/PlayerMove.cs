3440aedc0d3f2bc9b79ad26237704bdcfe63ecb2
Startuts <65373273+Startuts@users.noreply.github.com>
2020-05-19 16:56:27 +0100

    From the Player Movement

---
+++ Player Movement/Scripts/PlayerMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 100f;
+    public Rigidbody rb;
+    void Update()
+    {
+        float hor = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float ver = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        Vector3 move = new Vector3(hor, 0f, ver);
+
+        rb.AddForce(move);
+    }
+}
